b3f044ee1d3a670860bf3646a7f3b90dfdb74559
Wesley Werikis <wesleywerikis@outlook.com>
2024-03-25 16:34:24 -0300

    feat[structure]: class @SpringBootApplication and @SpringBootTest

---
+++ src/main/java/br/com/weswerikis/apicruduser/ApiCrudUserApplication.java
@@ -0,0 +1,13 @@
+package br.com.weswerikis.apicruduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/weswerikis/apicruduser/ApiCrudUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.weswerikis.apicruduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
