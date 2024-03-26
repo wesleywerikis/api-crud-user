3529b734deafd1e01c60c051c48c24a70b48bde3
Wesley Werikis <wesleywerikis@outlook.com>
2024-03-26 01:31:12 -0300

    feat[entity]: created entity User

---
+++ src/main/java/br/com/weswerikis/apicruduser/entity/User.java
@@ -0,0 +1,41 @@
+package br.com.weswerikis.apicruduser.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateDate;
+}
